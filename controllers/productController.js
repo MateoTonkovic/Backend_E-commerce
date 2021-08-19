3905ee625b4e34b7e77996ba3b0026d8b4e6aa04
maurogoyeneche <goyenechemauro@gmail.com>
2021-08-19 15:22:30 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,45 @@
 const { Admin, Product, User } = require("../models");
+const formidable = require("formidable");
 
 const store = async (req, res) => {
-  //   res.json("ok");
-  const product = await Product.create(req.body);
-  console.log(product);
-  res.json(req.body);
+  const form = formidable({
+    multiples: false,
+    uploadDir: "./public/img",
+    keepExtensions: true,
+  });
+  form.parse(req, async (err, fields, files) => {
+    console.log(files.image);
+    const path = require("path");
+    const imgName = path.basename(files.image.path);
+    if (files.image.name === "") {
+      const fs = require("fs");
+      fs.unlink(files.image.path, () => {});
+    }
+
+    // const [user, created] = await User.findOrCreate({
+    //   where: {
+    //     email: fields.email,
+    //   },
+    //   defaults: {
+    //     firstname: fields.firstname,
+    //     lastname: fields.lastname,
+    //   },
+    // });
+    const product = await Product.create(
+      {
+        name: fields.name,
+        description: fields.description,
+        photo: "/img/" + imgName,
+        stock: fields.stock,
+        bestproduct: fields.bestproducts,
+        slug: fields.slug,
+        price: fields.price,
+      },
+      { new: true }
+    );
+    res.json(product);
+    /*     sendMail(fields.title, fields.content); */
+  });
 };
 
 const index = async (req, res) => {
