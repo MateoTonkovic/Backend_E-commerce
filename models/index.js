f161577d37d47fb3db660ce304967ea6727535e7
pablomdh <pablodubh@gmail.com>
2021-08-21 13:09:00 -0300

    product order table

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+const { datatype } = require("faker");
 const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
@@ -17,6 +18,32 @@ const User = require("./User")(sequelize, Model, DataTypes);
 const Category = require("./Category")(sequelize, Model, DataTypes);
 const Product = require("./Product")(sequelize, Model, DataTypes);
 const Order = require("./Order")(sequelize, Model, DataTypes);
+const Order_Product = sequelize.define("Order_Product", {
+  orderId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Order,
+      key: "id",
+    },
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Product,
+      key: "id",
+    },
+  },
+  qty: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  unitPrice: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 
 //vinculamos los modelos
 Category.hasMany(Product);
@@ -36,4 +63,5 @@ module.exports = {
   Category,
   Product,
   Order,
+  Order_Product,
 };
